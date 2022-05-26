8bf18ada70503d44439ebea83278b1e6bfdfe786
Meet Ghodasara <meet.d.ghodasara@gmail.com>
2022-05-26 12:01:59 +0530

    Adding 4 diff operation in notes using yargs

--- Note App/app.js
+++ Note App/app.js
@@ -1,12 +1,39 @@
 const getNotes = require("./notes");
 const yargs = require("yargs");
+const { describe } = require("yargs");
 
-const command = process.argv[2];
+//Adding a new Note
+yargs.command({
+    command : 'add',
+    describe : 'Add a new note',
+    handler: function(){
+        console.log("Adding a new Notes");
+    }
+})
 
-// console.log(command);
+//Removing a new Note
 
-if (command === "add") {
-  console.log("Adding Notes!");
-} else if (command === "remove") {
-  console.log("Removing Notes!");
-}
+yargs.command({
+    command : 'remove',
+    describe : 'Remove a note',
+    handler: function(){
+        console.log("Removing a Note");
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:'List an all notes',
+    handler : function(){
+        console.log("Listing a notes");
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe:'Read a notes',
+    handler:function(){
+        console.log("Reading a note...");
+    }
+})
+console.log(yargs.argv);
