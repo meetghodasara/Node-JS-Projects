87ebc476b88bc2af3d6c1c41491b2d6367c86e5d
Meet Ghodasara <meet.d.ghodasara@gmail.com>
2022-05-26 13:23:38 +0530

    Adding  yargs builder

--- Note App/app.js
+++ Note App/app.js
@@ -6,8 +6,21 @@ const { describe } = require("yargs");
 yargs.command({
     command : 'add',
     describe : 'Add a new note',
-    handler: function(){
-        console.log("Adding a new Notes");
+    builder:{
+        title:{
+            describe:'Add a note',
+            demandOption :true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Add a description',
+            demandOption : true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log("Title : " + argv.title);
+        console.log("Body : " + argv.body);
     }
 })
 
@@ -36,4 +49,5 @@ yargs.command({
         console.log("Reading a note...");
     }
 })
-console.log(yargs.argv);
+// console.log(yargs.argv);
+yargs.parse();
