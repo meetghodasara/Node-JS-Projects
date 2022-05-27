f8709cca3f4fb7dffe369f9ad51961ffc1faea96
Meet Ghodasara <meet.d.ghodasara@gmail.com>
2022-05-27 16:44:16 +0530

    Delete weather-app from folder

---
+++ Note App/app.js
@@ -0,0 +1,73 @@
+const note = require("./notes");
+const yargs = require("yargs");
+const { describe } = require("yargs");
+const fs = require('fs');
+
+
+
+
+//Adding a new Note
+
+yargs.command({
+    command : 'add',
+    describe : 'Add a new note',
+    builder:{
+        title:{
+            describe:'Add a note',
+            demandOption :true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Add a description',
+            demandOption : true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        note.addNote(argv.title , argv.body);
+    }
+})
+
+//Removing a new Note
+
+yargs.command({
+    command : 'remove',
+    describe : 'Remove a note',
+    builder : {
+        title: {
+            describe:'Remove a note',
+            demandOption :true,
+            type: 'string'
+        },
+    },
+    handler(argv){
+       note.removeNote(argv.title);
+
+    }
+
+})
+
+yargs.command({
+    command:'list',
+    describe:'List an all notes',
+    handler(argv){
+        note.listNotes(argv.title);
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe:'Read a notes',
+    builder : {
+        title:{
+            describe:'Read a note',
+            demandOption:true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        note.readNote(argv.title);
+    }
+})
+// console.log(yargs.argv);
+yargs.parse();
---
+++ Note App/notes.js
@@ -0,0 +1,76 @@
+const fs = require("fs");
+
+const addNote = (title, body) => {
+    const notes = loadNote();
+    // const duplicateNotes = notes.filter((notes)=>{
+    //     return notes.title === title
+    // })
+    const duplicateNote = notes.find((note) => note.title === title);
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        });
+        saveNotes(notes);
+        console.log("Successfully saved a note");
+    } else {
+        console.log("The title is already taken !");
+    }
+};
+
+
+
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync("notes.json", dataJSON);
+};
+const loadNote = () => {
+    try {
+        const databuffer = fs.readFileSync("notes.json");
+        const dataJSON = databuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch {
+        return [];
+    }
+};
+
+const removeNote = (title) => {
+    const notes = loadNote();
+    const noteToKeep = notes.filter((note) => note.title !== title);
+
+    if (notes.length === noteToKeep.length) {
+        console.log("Note is not removed");
+    } else {
+        console.log("Note is removed");
+    }
+    saveNotes(noteToKeep);
+};
+
+const listNotes = () => {
+    const notes = loadNote();
+    console.log("::: Your notes :::");
+    notes.forEach((note) => {
+        console.log(note.title);
+    });
+};
+
+
+const readNote = (title) => {
+    const notes = loadNote();
+    const note = notes.find((note)=> note.title === title)
+
+    if(note !== undefined){
+        console.log(note.title);
+        console.log(note.body);
+    }else{
+        console.log('Note is not found');
+    }
+
+}
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote:readNote
+};
